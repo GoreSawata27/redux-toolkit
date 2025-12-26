4b9dfeacd60e80ad4d1bcf78485a8bd30b3ae03d
Sawata Gore <sawatagore27@gmail.com>
2025-12-26 14:37:37 +0530

    User login flow

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,25 +1,39 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { loginUser } from "@/store/features/auth/authSlice";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Login() {
   const [email, setEmail] = useState("test@test.com");
   const [password, setPassword] = useState("1234");
 
+  const dispatch = useDispatch();
+  const { isLoading, user, error } = useSelector((state) => state.auth);
+
+  const handelLogin = () => {
+    dispatch(loginUser({ email, password }));
+  };
+
   return (
-    <div className="flex flex-col gap-2">
-      <h1>Login Example</h1>
-      <div className="flex gap-2">
+    <div className="h-screen flex flex-col items-center justify-center gap-2">
+      <div className="w-full flex flex-col gap-2 max-w-100">
+        <h1 className="mb-10">Login </h1>
+        <Label>Email</Label>
         <Input value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+        <Label>Password</Label>
         <Input
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        <Button onClick={handelLogin}>Login</Button>
       </div>
-      <Button>Login</Button>
+
+      {error && <div>Something went wrong</div>}
     </div>
   );
 }
--- store/features/auth/authSlice.ts
+++ store/features/auth/authSlice.ts
@@ -1,1 +1,64 @@
-//
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+type loginData = {
+  email: string;
+  password: string;
+};
+
+const login = async ({ email, password }: loginData) => {
+  const payload = { email, password };
+  const res = await axios.post("http://localhost:3000/api/auth/login", payload);
+  return res.data;
+};
+
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async ({ email, password }: loginData, { rejectWithValue }) => {
+    try {
+      const apiRes = await login({ email, password });
+
+      return {
+        token: apiRes.token,
+        user: apiRes.user,
+      };
+    } catch (err) {
+      return rejectWithValue("Login failed");
+    }
+  }
+);
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    isLoading: false,
+    token: null,
+    user: null,
+    error: null,
+  },
+  reducers: {
+    logout: (state) => {
+      state.token = null;
+      state.user = null;
+    },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { logout } = authSlice.actions;
+export default authSlice.reducer;
