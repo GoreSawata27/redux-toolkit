43f726c903014b9685576cb7e2c0d54c0431dafc
Sawata Gore <sawatagore27@gmail.com>
2025-12-27 15:08:03 +0530

    apis uri

--- services/auth/authApi.ts
+++ services/auth/authApi.ts
@@ -2,7 +2,10 @@ import axios from "axios";
 import { loginData } from "@/store/features/auth/authTypes";
 
 export const loginApi = async ({ email, password }: loginData) => {
-  const res = await axios.post("http://localhost:3000/api/auth/login", { email, password });
+  const res = await axios.post("https://deploy-backend-h9zt.onrender.com/api/auth/login", {
+    email,
+    password,
+  });
 
   return res.data;
 };
